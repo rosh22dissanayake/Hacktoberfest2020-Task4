67bd9988d621f82282d1358f5de7a3564e5b174b
Roshan Dissanayake <56787847+rosh22dissanayake@users.noreply.github.com>
2020-10-31 12:05:14 +0530

    Update BubbleSort.c

--- BubbleSort.c
+++ BubbleSort.c
@@ -3,27 +3,28 @@
 
 int main()
 {
-    bool swap;
-    int n=5,j;
-    int arr[5]={58,89,71,35,6};
-    
-    
-    do{
-        swap=false;
-        for(j=0;j<n;j++)
-        {
-            if (arr[j]>arr[j-1]);
-            {
-                temp=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=temp
-                swap=false;
-            }
-        }
-    }while(swap);
-    
-    for(m=0;m<n;m--)
-        {
-            printf("%s ",arr[l]);
-        }
+    int a = 100;
+   int b = 200;
+   int ret;
+ 
+   
+   ret = max(a, b);
+ 
+   printf( "Max value is : %d\n", ret );
+ 
+   return 0;
+}
+ 
+
+int max(int num1, int num2) {
+
+   
+   int result;
+ 
+   if (num1 > num2)
+      result = num1;
+   else
+      result = num2;
+ 
+   return result; 
 }
